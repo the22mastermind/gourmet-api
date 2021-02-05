94f4634cb3380be0de7d14baef02f01afeb0ee4f
the22mastermind <bertrandmasabo22@gmail.com>
2021-02-05 11:26:38 +0200

    Added admin update order

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -3,17 +3,29 @@ import ordersController from '../controllers/orders';
 import authMiddleware from '../middlewares/authentication';
 import ordersMiddleware from '../middlewares/orders';
 
-const { getSpecificOrder, getOrdersList } = ordersController;
+const { getSpecificOrder, getOrdersList, updateOrder } = ordersController;
 const { checkUserToken, checkAdminRole } = authMiddleware;
 const {
   validateGetOrder,
   findOrderById,
   findOrdersList,
+  validateUpdateOrder,
+  checkOrderStatus,
 } = ordersMiddleware;
 
 const adminRoutes = express.Router();
 
-adminRoutes.get('/orders/:id', validateGetOrder, checkUserToken, checkAdminRole, findOrderById, getSpecificOrder );
-adminRoutes.get('/orders', checkUserToken, checkAdminRole, findOrdersList, getOrdersList );
+adminRoutes.get('/orders/:id', validateGetOrder, checkUserToken, checkAdminRole, findOrderById, getSpecificOrder);
+adminRoutes.get('/orders', checkUserToken, checkAdminRole, findOrdersList, getOrdersList);
+adminRoutes.patch(
+  '/orders/:id',
+  validateGetOrder,
+  validateUpdateOrder,
+  checkUserToken,
+  checkAdminRole,
+  findOrderById,
+  checkOrderStatus,
+  updateOrder,
+);
 
 export default adminRoutes;
--- src/validations/orders.js
+++ src/validations/orders.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 import messages from '../utils/messages';
 import authentication from './authentication';
 
-const {createErrorMessages} = authentication;
+const { createErrorMessages } = authentication;
 
 const orderContents = {
   itemId: Joi.number().positive().required().messages(
@@ -12,7 +12,8 @@ const orderContents = {
       `${messages.invalidItemId}`,
       `${messages.invalidItemId}`,
       `${messages.invalidItemId}`,
-    )),
+    ),
+  ),
   itemName: Joi.string().required().messages(
     createErrorMessages(
       'string',
@@ -20,40 +21,52 @@ const orderContents = {
       null,
       null,
       null,
-    )),
-  cost: Joi.number().min(1).precision(1).required().messages(
-    createErrorMessages(
+    ),
+  ),
+  cost: Joi.number().min(1).precision(1).required()
+    .messages(
+      createErrorMessages(
+        'number',
+        `${messages.emptyItemCost}`,
+        `${messages.invalidItemCost}`,
+        null,
+        `${messages.invalidItemCost}`,
+      ),
+    ),
+  quantity: Joi.number().min(1).positive().required()
+    .messages(createErrorMessages(
       'number',
-      `${messages.emptyItemCost}`,
-      `${messages.invalidItemCost}`,
+      `${messages.emptyItemQuantity}`,
+      `${messages.invalidItemQuantity}`,
       null,
-      `${messages.invalidItemCost}`,
+      `${messages.invalidItemQuantity}`,
     )),
-  quantity: Joi.number().min(1).positive().required().messages(createErrorMessages(
-    'number',
-    `${messages.emptyItemQuantity}`,
-    `${messages.invalidItemQuantity}`,
-    null,
-    `${messages.invalidItemQuantity}`,
-  )),
 };
 
+/**
+ * @description Validates the total, contents, and paymentId properties of req.body
+ * @param {object} data
+ * @returns {object} Validation result object
+ */
 const placeOrder = (data) => {
   const schema = Joi.object({
-    total: Joi.number().min(1).precision(1).required().messages(createErrorMessages(
-      'number',
-      `${messages.emptyTotal}`,
-      `${messages.invalidTotal}`,
-      `${messages.invalidTotal}`,
-      `${messages.invalidTotal}`,
-    )),
-    contents: Joi.array().items(orderContents).min(1).ordered(orderContents).required().messages(createErrorMessages(
-      'array',
-      `${messages.orderEmpty}`,
-      `${messages.orderEmpty}`,
-      null,
-      `${messages.orderEmpty}`,
-    )),
+    total: Joi.number().min(1).precision(1).required()
+      .messages(createErrorMessages(
+        'number',
+        `${messages.emptyTotal}`,
+        `${messages.invalidTotal}`,
+        `${messages.invalidTotal}`,
+        `${messages.invalidTotal}`,
+      )),
+    contents: Joi.array().items(orderContents).min(1).ordered(orderContents)
+      .required()
+      .messages(createErrorMessages(
+        'array',
+        `${messages.orderEmpty}`,
+        `${messages.orderEmpty}`,
+        null,
+        `${messages.orderEmpty}`,
+      )),
     paymentId: Joi.string().required().messages(createErrorMessages(
       'string',
       `${messages.emptyPaymentId}`,
@@ -68,6 +81,11 @@ const placeOrder = (data) => {
   });
 };
 
+/**
+ * @description Validates the id parameter of req.params
+ * @param {object} data
+ * @returns {object} Validation result object
+ */
 const getOrder = (data) => {
   const schema = Joi.object({
     id: Joi.number().positive().required().messages(createErrorMessages(
@@ -84,7 +102,29 @@ const getOrder = (data) => {
   });
 };
 
+/**
+ * @description Validates if the status property of is one of (accepted, onthemove, or completed
+ * @param {object} data
+ * @returns {object} Validation result object
+ */
+const updateOrder = (data) => {
+  const schema = Joi.object({
+    status: Joi.string().regex(/^(accepted|onthemove|completed)$/).required().messages(createErrorMessages(
+      'string',
+      `${messages.orderUpdateEmptyStatus}`,
+      `${messages.orderUpdateInvalidStatus}`,
+      `${messages.orderUpdateInvalidStatus}`,
+      `${messages.orderUpdateInvalidStatus}`,
+    )),
+  });
+  return schema.validate(data, {
+    abortEarly: false,
+    allowUnknown: false,
+  });
+};
+
 export default {
   placeOrder,
   getOrder,
+  updateOrder,
 };
